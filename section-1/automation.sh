9fec7e4304929bc14f127afebbac05f4d176809d
Abdul Wahid Muhaemin <wamuhabdul@gmail.com>
2023-11-08 14:03:37 +0700

    [FEATURE]: section 1 create automation shell scripting

---
+++ section-1/automation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# print a message that the script has started executing
+echo "Starting automation script."
+
+# create a directory named "new_folder"
+mkdir new_folder
+
+# change into the "new_folder" directory
+cd new_folder
+
+# create a file named "example.txt" and write "Hello, world!" into it
+echo "Hello, world!" > example.txt
+
+# see all the files in the "new_folder" directory
+ls
+
